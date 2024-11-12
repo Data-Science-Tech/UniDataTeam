86db564893a3da62a1ddb88e3aa1fd95b1645eb3
Jerry-Desktop <unclefish7@outlook.com>
2024-11-12 18:39:32 +0800

    train_model更改了标注的查询方式，但未验证

--- backend/springbootquickstart/src/main/resources/python/train_model.py
+++ backend/springbootquickstart/src/main/resources/python/train_model.py
@@ -67,15 +67,19 @@ class DatabaseDataset(Dataset):
 
         # 如果是训练集，获取标注
         if self.is_training and self.car_category_id is not None:
-            sample_id = self.samples[idx]
+            sensor_data_id = self.data[idx][0]
 
-            # 查询类别为Car的标注
-            self.cursor.execute("""  
-                SELECT sa.bbox_2d_xmin, sa.bbox_2d_ymin, sa.bbox_2d_xmax, sa.bbox_2d_ymax  
-                FROM sample_annotation sa  
-                JOIN instance i ON sa.instance_id = i.instance_id  
-                WHERE sa.sample_id = %s AND i.category_description_id = %s  
-            """, (sample_id, self.car_category_id))
+            # 查询类别为 Car 的标注
+            self.cursor.execute("""
+            SELECT a2d.bbox_2d_xmin, a2d.bbox_2d_ymin, a2d.bbox_2d_xmax, a2d.bbox_2d_ymax
+            FROM sensor_data sd
+            JOIN sample_info si ON sd.sample_id = si.sample_id
+            JOIN sample_annotation sa ON si.sample_id = sa.sample_id
+            JOIN annotation_2d a2d ON sa.annotation_id = a2d.sample_annotation_id
+            JOIN instance i ON sa.instance_id = i.instance_id
+            WHERE sd.sensor_data_id = %s 
+            AND i.category_description_id = %s
+            """, (sensor_data_id, self.car_category_id))
             annotations = self.cursor.fetchall()
 
             # 转换为(xmin, ymin, xmax, ymax)格式
