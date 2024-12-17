2ea1ba8f59859739fe24e82626fc32e45e498483
QC <qincheng037@gmail.com>
2024-12-17 16:49:40 +0800

    修改bug，完成测试

--- frontend/sakai-vue/src/Api/GetAllTasksApi.js
+++ frontend/sakai-vue/src/Api/GetAllTasksApi.js
@@ -10,7 +10,7 @@ const apiClient = axios.create({
 
 export default {
     // 根据id获取所有热任务
-    getalltests(id) {
+    getalltests(userId) {
         return apiClient.get(`/api/task_management/get_user_server_usage_by_userid/${userId}`);
       }
   };
